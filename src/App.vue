07a63c9354bac760cd406115e979dc2b61af4f06
cheni0915 <easy09150915@gmail.com>
2024-07-31 16:00:00 +0800

    update code

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// script setup 寫法和cdn方式不同，不用return 
+
+import { ref } from 'vue'
+const num = ref(2)
+//   console.log(num);
+console.log(num.value)
+
+function clickMe() {
+  num.value++
+}
+
+// return { num, clickMe };
+</script>
+
+<template>
+  <div>
+    {{ num }}
+    <button type="button" v-on:click="clickMe">點我</button>
+  </div>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // base: '' 路徑更改 (dist 資料夾)
+  // vue router 情況， base:''/Server/  較建議
+  base: '/',
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
